a45404bfb60a01a30ec56e72040716989c1b0860
Jake Overall <joverall22@gmail.com>
2018-12-04 11:41:51 -0700

    Student Handoff

--- src/store.js
+++ src/store.js
@@ -1,16 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+
+let movieApi = Axios.create({
+  baseURL: 'https://api.themoviedb.org/3/search/movie?api_key=606e6aee588b47993fffe6d9530d07a6&page=1&include_adult=false&query=',
+  timeout: 3000
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    searchResults: [],
+    activeMovie: {}
   },
   mutations: {
-
+    setResults(state, results) {
+      state.searchResults = results
+    },
+    setActiveMovie(state, movie) {
+      state.activeMovie = movie
+    }
   },
   actions: {
-
+    search({ commit, dispatch }, query) {
+      movieApi.get(query)
+        .then(res => {
+          let data = res.data.results
+          commit('setResults', data)
+        })
+    },
+    setActiveMovie({ commit, dispatch }, movie) {
+      commit('setActiveMovie', movie)
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-fluid">
+    <div class="row">
+      <div class="col">
+        <h1>Welcome to Vue-Flix</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p>Searching for: {{query}}</p>
+        <form @submit.prevent="search">
+          <input type="text" placeholder="Search" v-model='query'>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+    <!-- Components Here  -->
+    <div class="row justify-content-around mt-5 pb-5">
+      <div class="col-5">
+        <results></results>
+      </div>
+      <div class="col-5">
+        <preview></preview>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  import Results from '@/components/Results.vue'
+  import Preview from '@/components/Preview.vue'
+  export default {
+    name: 'home',
+    data() {
+      return {
+        query: ''
+      }
+    },
+    computed: {
+      results() {
+        return this.$store.state.searchResults
+      }
+    },
+    methods: {
+      search() {
+        this.$store.dispatch('search', this.query)
+      }
+    },
+    components: {
+      Results,
+      Preview
+    }
   }
-}
 </script>
+
+<style>
+  .home {
+    background-color: green
+  }
+</style>
